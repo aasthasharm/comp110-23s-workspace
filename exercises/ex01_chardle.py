93c5226e3c126c982e3f1ba0c767f4f0af7895f6
Aastha <sharm@ad.unc.edu>
2023-01-26 04:39:41 -0500

    Progress on Exercise 1

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -1,26 +1,29 @@
 """EX01 - Chardle - A cute step toward Wordle."""
-__author__: str = 730577151
+__author__: str = '730577151'
 
 word_input: str = input("Enter a 5-character word: ")
 
 if len(word_input) != 5:
-    print("Word must contain 5 characters")
+    print("Error: Word must contain 5 characters")
+    exit()
 else: 
     char_input: str = input("Enter a single character: ") 
     number_of_matches: int = 0
 
     if len(char_input) != 1:
-        print("Character must be a single character")
+        print("Error: Character must be a single character")
+        exit()
     else:
         print("Searching for " + str(char_input) + " in " + str(word_input))
 
-        for n in range (5):
+        for n in range(5):
             if str(char_input) == word_input[n]:
                 print(str(char_input) + " found at index " + str(n))
-                number_of_matches = number_of_matches +1
+                number_of_matches = number_of_matches + 1
 
         if number_of_matches == 1:
-            print("there is " + str(number_of_matches) + " instance of " + str(char_input) + " in " + str(word_input))
+            print(str(number_of_matches) + " instance of " + str(char_input) + " found in " + str(word_input))
+        elif number_of_matches == 0:
+            print("No instances of " + str(char_input) + " found in " + str(word_input))
         else:
-            print("there are " + str(number_of_matches) + " instances of " + str(char_input) + " in " + str(word_input))
-
+            print(str(number_of_matches) + " instances of " + str(char_input) + " found in " + str(word_input))
